d2b284a60ae4cd228056e4a6b190cb120f6fca02
SniperPRX <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-05-10 03:02:48 +0100

    Update tokenRing.c

--- tokenRing.c
+++ tokenRing.c
@@ -48,20 +48,20 @@ int main(int argc,char *argv[])
         }
     }
     else{
-        char* nome_mknod = "mk1";
-        char* nome_mknod2 = "mk2";
-        mknod(nome_mknod, FIFO1, S_IFIFO | PERMS, 0);
+        mknod(FIFO1, S_IFIFO | PERMS, 0);
         pipe1[1] = open(FIFO1,1);
         int pipetasDeEscrita[numDePipetas ][2];
         for(int i = 0;i<numDePipetas  ;i++){
             char * bufferEsc = malloc(15);
             sprintf(bufferEsc,"/tmp/f%d",i+2);
             //limpar
-            mknod(nome_mknod2, bufferEsc,S_IFIFO | PERMS ,0);
+            mknod(bufferEsc,S_IFIFO | PERMS ,0);
             pipetasDeEscrita[i][1] = open(bufferEsc,1);
             memset(bufferEsc,0,15);
+            //unlink(bufferEsc);
             free(bufferEsc);
         }
+    
         int token=0;
         int i=0;
         while (token<= max){  
@@ -86,23 +86,12 @@ int main(int argc,char *argv[])
         
             token++;
         }
+        //unlink(FIFO1);
         close(pipe1[1]);
-        int res = unlink(nome_mknod);
-        int res2 = unlink(nome_mknod2);
-        if (res == -1) {
-            perror("Falha ao desvincular mknod 1");
-            return 1;
-        }
-        if (res2 == -1) {
-            perror("Falha ao desvincular mknod 2");
-            return 1;
-        }
-        printf("Mknods desvinculado com sucesso!\n");
-        
+   
         for(int i = 0; i<numDePipetas ; i++){
             close(pipetasDeEscrita[i][1]);
         }
-
         wait(NULL);
     }
     return 0;
