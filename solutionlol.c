ffe893df1e12223b49c4f4420c131fb41d7470b5
SniperPRX <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-05-18 19:02:11 +0100

    Update solutionlol.c

--- solutionlol.c
+++ solutionlol.c
@@ -11,34 +11,26 @@
 #include <signal.h>
 #define PERMS 0666
 #define DEBUGGING 0
-
- int token = -1;
-
 #define TRUE 1
+
+int token = -1;
 int parent;
 int *ids;
 int ** pipetas;
 volatile char lock =0;
-sem_t sem;
 
-/*void stopHandler(int signum){
-    printf("LOL");
-    for(int k=0;k<sizeof(ids)/sizeof(ids[0]);k++ ){
-                     kill(ids[k],SIGINT);
-                } 
-    exit(1);
-}*/
+sem_t sem;
 
 void trancar(){
     label:{
-    char local = 1;
-    char tmp = lock;
-    lock = local;
-    local =tmp;
+        char local = 1;
+        char tmp = lock;
+        lock = local;
+        local =tmp;
     
-    if(0 != local){
-        goto label;
-    }
+        if(0 != local){
+            goto label;
+        }
     }
 }
 
@@ -62,91 +54,71 @@ int main (int argc,char * argv[] )
     int len = sizeof(int *) * numDePipetas + sizeof(int) * 2 * numDePipetas;
     pipetas = (int **) malloc(len);
     int * ptr = ( int *) (pipetas+numDePipetas);
-    for(int i = 0; i< numDePipetas; i++)
-    {
+
+    for(int i = 0; i< numDePipetas; i++){
         pipetas[i] = (ptr + 2 * i);
     }
     
     for(int i = 0; i< numDePipetas; i++){
         char * buffer = (char *) malloc(15);
+
         if(i!=numDePipetas-1){
-        sprintf(buffer,"pipe%dto%d",i+1,i+2);}
-        else  sprintf(buffer,"pipe%dto%d",numDePipetas,1);
+            sprintf(buffer,"pipe%dto%d",i+1,i+2);
+        }else  sprintf(buffer,"pipe%dto%d",numDePipetas,1);
+
         mkfifo(buffer,PERMS);
         strncpy(nomes[i],buffer,15);
         memset(buffer,0,15);
         free(buffer);
+
         if(DEBUGGING){ printf("CRIAR PIPE\n");}
     }
+    
     for(int i = 0; i< numDePipetas  ;i ++){
         pid = fork();
         if(pid == 0){ // processo filho
-        if(DEBUGGING){ printf("CRIAR FILHO\n");}
-        ids[i] = getpid();
-        unsigned int seed = ids[i];
-        srand(seed);
-        if(DEBUGGING){ printf("STACK?\n");}
-        //abrir pipes
+            if(DEBUGGING){ printf("CRIAR FILHO\n");}
+            ids[i] = getpid();
+            unsigned int seed = ids[i];
+            srand(seed);
+            if(DEBUGGING){ printf("STACK?\n");}
+            //abrir pipes
         
-        pipetas[i][0] = open(nomes[i], O_RDONLY);
-        pipetas[i][1] = open(nomes[(i + 1) % numDePipetas],O_WRONLY);
+            pipetas[i][0] = open(nomes[i], O_RDONLY);
+            pipetas[i][1] = open(nomes[(i + 1) % numDePipetas],O_WRONLY);
         
-   
+            if(DEBUGGING){ printf("STACK?\n");}
         
-        if(DEBUGGING){ printf("STACK?\n");}
-        
+            while(1){
+                read(pipetas[i][0], &token, sizeof(int));
+                // printf("[p%d] received token (val = %d)\n", i + 1, token);
 
-        while(1){
-            
-            read(pipetas[i][0], &token, sizeof(int));
-            // printf("[p%d] received token (val = %d)\n", i + 1, token);
-            
-
-            //srand(time(NULL));
-            if(  (rand() % 100 + 1 ) < probInt){
-                printf("[p%d] blocked on token (val = %d) PROCESSO = %d\n",i+1,token, getpid());
-                sleep(tempoDeEspera);
-            }
-
-             
-            
-             if(token >= max){
-               // if(1) printf("\n FechoPID:%d \n",getpid());
-                for(int k=0;k<numDePipetas;k++){
-                    
-                    close(pipetas[k][1]);
-                    close(pipetas[k][0]);
-                    unlink(nomes[k]);
-                    
-                    
+                if((rand() % 100 + 1 ) < probInt){
+                    printf("[p%d] blocked on token (val = %d) PROCESSO = %d\n",i+1,token, getpid());
+                    sleep(tempoDeEspera);
                 }
-                
-                for(int k=0;k<numDePipetas;k++ ){
 
-                    //printf("Numero de pipes=%d\n",numDePipetas);
-                    //printf("id %d : %d\n",k+1,ids[k]);
-                    if(k!=i)
-                     kill(ids[k],SIGINT);
-                } 
-               
+                if(token >= max){
+                    for(int k=0;k<numDePipetas;k++){
+                        close(pipetas[k][1]);
+                        close(pipetas[k][0]);
+                        unlink(nomes[k]);
+                    }
                 
-                return 0;
-                              
-             }
+                    for(int k=0;k<numDePipetas;k++ ){
+                        if(k!=i){kill(ids[k],SIGINT);}
+                    } 
+                
+                    return 0;         
+                }
              
-            
-            int novoToken = token + 1;
-         
-           
-             write(pipetas[i][1], &novoToken, sizeof(int)); 
-
-
-           }
+                int novoToken = token + 1;
+                write(pipetas[i][1], &novoToken, sizeof(int)); 
+            }
 
         }
       
     }
-
 
     int fd_write = open("pipe1to2", O_WRONLY);
     token = 0;
